640b76fcde9b73b4064f5d9f86ae2e0a84da027c
Yano Cavalcante <yano.melo.cavalcante@gmail.com>
2024-11-20 21:08:56 -0300

    feat: adding all multiplexers

--- datapath.vhd
+++ datapath.vhd
@@ -162,20 +162,19 @@ end component;
 --============================================================--
 
 -- MEUS SINAIS
-signal SwSelecao <= SW(7 downto 0);
-signal BitsSelecao: std_logic_vector(7 downto 0);
-signal SwInputUser <= SW(9 downto 0);
-signal BitsInput: std_logic_vector(9 downto 0);
-signal ResultComparada: std_logic_vector(9 downto 0);
-signal BitsSomadosVerifica, BitsSomadosResultado: std_logic_vector(3 downto 0);
-signal entrada1_mux_8_0 <= "1010" & ResultComparada;
-signal entrada2_mux_8_0 <= "000" & end_game & (not(Round));
-signal enable_aux_reg <= (E5 or E4);
+signal SwInputUser, BitsInput, Code, ResultComparada, EntradaLEDS: std_logic_vector(9 downto 0);
+signal BitsSelecao, SwSelecao, saida_aux_reg: std_logic_vector(7 downto 0);
+signal saida_mux_eight: std_logic_vector(7 downto 0);
+signal entrada1_mux_eight, entrada2_mux_eight: std_logic_vector(7 downto 0)
+signal Tempo, t, r, n, allOnes: std_logic_vector(6 downto 0);
+signal saida_decoder_hex_4, saida_decoder_2_hex_2, saida_decoder_1_hex_2: std_logic_vector(6 downto 0);
+signal saida_mux_seven_0, saida_mux_seven_1, saida_mux_seven_2, saida_mux_seven_3, saida_mux_seven_4, saida_mux_seven_5: std_logic_vector(6 downto 0);
+signal Round, BitsSomadosVerifica, BitsSomadosResultado, saida_mux_four: std_logic_vector(3 downto 0);
+signal enable_mux_seven_1_and_3, enable_mux_seven_4_and_5, enable_aux_reg: std_logic;
 
 -- SINAIS PRÉ-PRONTOS
 signal selMux23, selMux45, end_game_interno, end_round_interno, clk_1, enableRegFinal: std_logic; --1 bit
 signal Round, Level_time, Level_code, SaidaCountT, SomaDigitada, SomaSelDig, CounterTMux: std_logic_vector(3 downto 0); -- 4 bits
-signal decMuxCode, decMuxRound, muxMux2, muxMux3, decMux4, Tempo, t, r, n: std_logic_vector(6 downto 0); -- 7 bits
 signal SomaSelDig_estendido,SeqLevel, RegFinal, valorfin_vector, MuxSelDig: std_logic_vector(7 downto 0); -- 8 bits
 signal N_unsigned: unsigned(3 downto 0);
 signal SeqDigitada, ComparaSelDig, SelecionadaROM, EntradaLEDS: std_logic_vector(9 downto 0); -- 10 bits
@@ -210,7 +209,7 @@ REG_10_BITS: reg10bits port map (
 );
 
 REG_AUX_8_BITS: reg8bits port map (
-    saida_mux_8_0,
+    saida_mux_eight,
     enable_aux_reg,
     CLK,
     R2,
@@ -225,11 +224,67 @@ MY_ROM: rom port map (
 
 ---------------------MULTIPLEXADORES----------------------------
 
-MUX_EIGHT_0: mux2pra1_8bits port map (
-    entrada1_mux_8_0,
-    entrada2_mux_8_0,
+MUX_EIGHT: mux2pra1_8bits port map (
+    entrada1_mux_eight,
+    entrada2_mux_eight,
     E5,
-    saida_mux_8_0
+    saida_mux_eight
+);
+
+MUX_TEN: mux2pra1_10bits port map (
+    "0000000000",
+    Code,
+    EntradaLEDS
+);
+
+MUX_FOUR: mux2pra1_4bits port map (
+    Tempo,
+    BitsSelecao(7 downto 0),
+    E2,
+    saida_mux_four
+);
+
+--Multiplexadores de 7 bits
+MUX_SEVEN_5: mux2pra1_7bits port map (
+    t,
+    allOnes,
+    enable_mux_seven_4_and_5,
+    saida_mux_seven_5
+);
+
+MUX_SEVEN_4: mux2pra1_7bits port map (
+    saida_decoder_hex_4,
+    allOnes,
+    enable_mux_seven_4_and_5,
+    saida_mux_seven_4
+);
+
+MUX_SEVEN_3: mux2pra1_7bits port map (
+    r,
+    saida_mux_seven_2,
+    enable_mux_seven_1_and_3,
+    saida_mux_seven_3
+);
+
+MUX_SEVEN_2: mux2pra1_7bits port map (
+    n,
+    allOnes,
+    E1,
+    saida_mux_seven_2
+);
+
+MUX_SEVEN_1: mux2pra1_7bits port map (
+    saida_decoder_2_hex_2,
+    saida_mux_seven_0,
+    enable_mux_seven_1_and_3,
+    saida_mux_seven_1
+);
+
+MUX_SEVEN_0: mux2pra1_7bits port map (
+    saida_decoder_1_hex_2,
+    allOnes,
+    E1,
+    saida_mux_seven_0
 );
 -------------------COMPARADORES E SOMA--------------------------
 
@@ -248,7 +303,7 @@ SOMA_VERIFICA: soma port map (
 
 -- Comparador que verifica se o jogador colocou 4 '1s' lógicos
 COMPARA_VERIFICA: comp_igual4 port map (
-    BitSomadosVerifica,
+    BitsSomadosVerifica,
     sw_erro
 );
 
@@ -265,17 +320,41 @@ COMPARA_RESULTADO: comp_igual4 port map (
 );
         
 ---------------------DECODIFICADORES----------------------------
+DECODER_HEX_4: decod7seg port map (
+    saida_mux_four,
+    saida_decoder_hex_4
+)
+DECODER_2_HEX_2: decod7seg port map (
+    BitsSelecao(3 downto 0),
+    saida_decoder_2_hex_2
+);
 
-DECODER_HEX_0: decod7seg port map (
-
+DECODER_1_HEX_2: decod7seg port map (
+    Round,
+    saida_decoder_1_hex_2
 );
 
 DECODER_HEX_1: decod7seg port map (
+    saida_aux_reg(t downto 4);
+    HEX1
+);
 
+DECODER_HEX_0: decod7seg port map (
+    saida_aux_reg(3 downto 0),
+    HEX0
 );
 
 ---------------------ATRIBUICOES DIRETAS---------------------
+SwSelecao <= SW(7 downto 0);
+SwInputUser <= SW(9 downto 0);
 
--- a fazer pel@ alun@
+allOnes <= "1111111"
 
+entrada1_mux_eight <= "1010" & BitsSomadosResultado;
+entrada2_mux_eight <= "000" & end_game & (not Round); 
+
+enable_aux_reg <= (E5 or E4);
+enable_mux_seven_1_and_3 <= (R1 xor R2);
+
+LEDR <= EntradaLEDS
 end arc;
