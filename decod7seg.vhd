201c754b032a8246160ebf22cafde665c46db704
Yano Cavalcante <yano.melo.cavalcante@gmail.com>
2024-11-22 21:48:09 -0300

    fix: adding decoding for hexas

--- decod7seg.vhd
+++ decod7seg.vhd
@@ -10,11 +10,10 @@ end decod7seg;
 
 architecture decod of decod7seg is
 begin
-    -- Decodificação para os números de 0 a 9 e a letra A
     HEX <= "1000000" when C = "0000" else -- 0
             "1001111" when C = "0001" else -- 1
             "0010010" when C = "0010" else -- 2
-            "0000110" when C = "0011" else -- 3
+            "0110000" when C = "0011" else -- 3
             "1001100" when C = "0100" else -- 4
             "0100100" when C = "0101" else -- 5
             "0100000" when C = "0110" else -- 6
@@ -22,5 +21,10 @@ begin
             "0000000" when C = "1000" else -- 8
             "0000100" when C = "1001" else -- 9
             "0001000" when C = "1010" else -- A
+            "0000011" when C = "1011" else -- b
+            "1000110" when C = "1100" else -- C
+            "0100001" when C = "1101" else -- d
+            "0000110" when C = "1110" else -- E
+            "0001110" when C = "1111" else -- F
             "1111111"; -- Estado padrão (todos apagados)
 end decod;
